100317a1ac1a354143b6c17e409bd40183b919ba
Fayti1703 <Fayti1703@loop.de>
2013-11-30 18:22:09 +0100

    Compression and Player isAlive() checking. I am derping alot, don't I?

--- scripts/fireBoom.js
+++ scripts/fireBoom.js
@@ -1,9 +1,7 @@
-if(player.fireLevel >= 0) {
-    if(inv.getCurrentItem() != null) {
+if(player.fireLevel >= 0 && inv.getCurrentItem() != null && player.isAlive()) {
         if(inv.getCurrentItem().itemID == 466 || inv.getOffhandItem().itemID == 466) {
             var Bomb = world.spawnEntity("Bomb",pos.x,pos.y,pos.z)
             Bomb.setFireLevel(100)
             inv.consumeItem(466)
         }
-    }
 }
