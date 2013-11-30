5908b67f225941aa670220ae2a7e5fb7b6f7e642
Fayti1703 <Fayti1703@loop.de>
2013-11-30 17:57:09 +0100

    Made the "Bomb" entity local on fireBoom.js. Ooops.

--- scripts/fireBoom.js
+++ scripts/fireBoom.js
@@ -1,6 +1,6 @@
 if(player.fireLevel <= 0) {
     if(inv.getCurrentItem().itemID == 466 || inv.getOffhandItem().itemID == 466) {
-        Bomb = world.spawnEntity("Bomb",pos.x,pos.y,pos.z)
+        var Bomb = world.spawnEntity("Bomb",pos.x,pos.y,pos.z)
         Bomb.setFireLevel(100)
         inv.consumeItem(466)
     }
