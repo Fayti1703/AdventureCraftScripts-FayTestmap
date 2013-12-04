b2784e6806cc726e25deb372607aa55a9e04f599
Fayti1703 <Fayti1703@loop.de>
2013-12-04 19:12:12 +0100

    Did some more stuff with Target.And more ent-stuff
    Actually I just added a status var, and well, Target.

---
+++ scripts/Target_onDeath.js
@@ -0,0 +1,1 @@
+status[entity.entityID].Body.removeFromScreen()
--- scripts/Target_onSpawned.js
+++ scripts/Target_onSpawned.js
@@ -5,10 +5,11 @@
 // If you find any problems with this template feel free to edit it
 // but I'd appreciate it if you'd tell me about it so everyone can benefit :)
 // - ZeuX
+status[entity.entityID] = ["ArrayStatusEntity"]
+status[entity.entityID].Body = new Model();
+status[entity.entityID].Body.addBox("Body", -8, 28, 0, 16, 16, 1, -1, -1);
+status[entity.entityID].Body.setPosition(0, 0, 0);
+status[entity.entityID].Body.setRotation(0, 0, 0);
 
-var Body = new Model();
-Body.addBox("Body", -8, 28, 0, 16, 16, 1, -1, -1);
-Body.setPosition(0, 0, 0);
-Body.setRotation(0, 0, 0);
-
-Body.texture = "customEntity/target.png"
+status[entity.entityID].Body.texture = "customEntity/target.png"
+status[entity.entityID].Body.attachedTo = entity
