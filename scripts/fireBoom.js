72d1e114ceff8d300e9b5f8879fe06ad862c77c5
Fayti1703 <Fayti1703@loop.de>
2013-12-04 19:20:01 +0100

    Derpy,derpy fire Boom.

--- scripts/fireBoom.js
+++ scripts/fireBoom.js
@@ -1,5 +1,5 @@
 if(player.fireLevel >= 0 && player.isAlive()) {
-        if(inv.getCurrentItem() != null && inv.getCurrentItem().itemID == 466 || inv.getOffHandItem() != null && inv.getOffhandItem().itemID == 466) {
+        if(inv.getCurrentItem() != null && inv.getCurrentItem().itemID == 466 || inv.getOffhandItem() != null && inv.getOffhandItem().itemID == 466) {
             var Bomb = world.spawnEntity("Bomb",pos.x,pos.y,pos.z)
             Bomb.setFireLevel(100)
             inv.consumeItem(466)
